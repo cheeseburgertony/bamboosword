962c685109399439c626d9732e864b91cfd73c11
cheeseburgertony <1821955020@qq.com>
2024-08-10 11:58:02 +0800

    feat: Menu组件-SubMenu下拉菜单添加测试

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,6 +1,8 @@
 import { fireEvent, render, screen } from "@testing-library/react"
 import Menu, { MenuProps } from "./menu"
 import MenuItem from "./menu-item"
+import SubMenu from "./sub-menu"
+import { wait } from "@testing-library/user-event/dist/utils"
 
 
 const testProps: MenuProps = {
@@ -25,9 +27,29 @@ const generateMenu = (props: MenuProps) => {
       <MenuItem>
         xyz
       </MenuItem>
+      <SubMenu title="dropdown">
+        <MenuItem>
+          drop1
+        </MenuItem>
+      </SubMenu>
     </Menu>
   )
 }
+
+// const createStyleFile = () => {
+//   const cssFile: string = `
+//     .bamboosword-submenu {
+//       display: none;
+//     }
+//     .bamboosword-submenu.menuopened{
+//       display: block;
+//     }
+//   `
+//   const style = document.createElement('style')
+//   style.type = 'text/css'
+//   style.innerHTML = cssFile
+//   return style
+// }
 
 
 describe('test Menu and MenuItem component', () => {
@@ -36,6 +58,7 @@ describe('test Menu and MenuItem component', () => {
     const menuElement = screen.getByTestId('test-menu');
     const activeElement = screen.getByText('active');
     const disabledElement = screen.getByText('disabled');
+    // expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4)
     expect(menuElement).toBeInTheDocument();
     expect(activeElement).toHaveClass('menu-item is-active');
     expect(disabledElement).toHaveClass('menu-item is-disabled');
@@ -48,14 +71,29 @@ describe('test Menu and MenuItem component', () => {
     fireEvent.click(thirdItem)
     expect(thirdItem).toHaveClass('is-active')
     expect(activeElement).not.toHaveClass('is-active')
-    expect(testProps.onSelect).toHaveBeenCalledWith(2)
+    expect(testProps.onSelect).toHaveBeenCalledWith('2')
     fireEvent.click(disabledElement)
     expect(disabledElement).not.toHaveClass('is-active')
-    expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
+    expect(testProps.onSelect).not.toHaveBeenCalledWith('1')
   })
   it('should render vertical mode when mode is set to vertival', () => {
     render(generateMenu(testVerProps));
     const menuElement = screen.getByTestId('test-menu');
     expect(menuElement).toHaveClass('menu-vertical')
+  })
+  it('should show dropdonw items when hover on subMenu', async () => {
+    // const wrapper = render(generateMenu(testProps));
+    // wrapper.container.append(createStyleFile())
+    // expect(screen.getByText('drop1')).not.toBeVisible()
+    // const dropdownElement = screen.getByText('dropdown')
+    // await wait(() => {
+    //   expect(screen.queryByText('drop1')).toBeVisible()
+    // })
+    // fireEvent.click(screen.getByText('drop1'))
+    // expect(testProps.onSelect).toHaveBeenCalledWith('3-0')
+    // fireEvent.mouseLeave(dropdownElement)
+    // await wait(() => {
+    //   expect(screen.queryByText('drop1')).not.toBeVisible()
+    // })
   })
 })
