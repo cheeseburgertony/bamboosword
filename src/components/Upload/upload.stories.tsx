4dbdeefd48256961cf0eb2f8bdaf74472e9435fc
cheeseburgertony <1821955020@qq.com>
2024-08-12 23:06:05 +0800

    feat: Upload组件-添加story

--- src/components/Upload/upload.stories.tsx
+++ src/components/Upload/upload.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { Upload } from './upload'
 import Icon from '../Icon/icon'
+import Button from '../Button/button'
 
 const meta: Meta<typeof Upload> = {
   title: 'Upload',
@@ -12,34 +13,55 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-const checkSize = (file: File) => {
-  if (Math.round(file.size / 1024) > 50) {
-    alert('file too big')
-    return false
-  } else {
-    return true
-  }
-}
-
-const filePromise = (file: File) => {
-  const newFile = new File([file], 'new_name.docx', { type: file.type })
-  return Promise.resolve(newFile)
-}
-
-export const Default: Story = {
-  render: () => (
+export const SimpleUpload: Story = {
+  render: (args) => (
     <Upload
+      {...args}
       action='https://run.mocky.io/v3/34786f50-6906-4010-87fd-2a8d83b7f967'
-      name='fileName'
-      data={{ 'key': 'value' }}
-      headers={{ 'X-Powered-By': 'bamboosword' }}
-      accept='.jpg'
-      multiple
-      drag
     >
-      <Icon icon="upload" size="5x" theme="secondary" />
-      <br />
-      <p>点击或者拖动到此区域进行上传</p>
+      <Button size="lg" btnType="primary"><Icon icon="upload" /> 点击上传 </Button>
     </Upload>
   )
 }
+SimpleUpload.storyName = '普通的Upload组件'
+
+export const CheckUpload: Story = {
+  render: (args) => {
+    const checkFileSize = (file: File) => {
+      if (Math.round(file.size / 1024) > 50) {
+        alert('file too big')
+        return false;
+      }
+      return true;
+    }
+    return (
+      <Upload
+        {...args}
+        action='https://run.mocky.io/v3/34786f50-6906-4010-87fd-2a8d83b7f967'
+        beforeUpload={checkFileSize}
+      >
+        <Button size="lg" btnType="primary"><Icon icon="upload" /> 不能上传大于50Kb！的文件 </Button>
+      </Upload>
+    )
+  }
+}
+CheckUpload.storyName = '上传前检查文件大小'
+
+export const DragUpload: Story = {
+  render: (args) => {
+    return (
+      <Upload
+        {...args}
+        action='https://run.mocky.io/v3/34786f50-6906-4010-87fd-2a8d83b7f967'
+        name="fileName"
+        multiple
+        drag
+      >
+        <Icon icon="upload" size="5x" theme="secondary" />
+        <br />
+        <p>点击或者拖动到此区域进行上传</p>
+      </Upload>
+    )
+  }
+}
+DragUpload.storyName = '拖动上传'
