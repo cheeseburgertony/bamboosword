181c6c59a88cdbdec7ec3cab46e3d0cb4c424695
cheeseburgertony <1821955020@qq.com>
2024-08-13 01:41:28 +0800

    feat: Form组件实现

---
+++ src/components/Form/form.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Form } from './form'
+
+const meta: Meta<typeof Form> = {
+  title: 'Form',
+  component: Form,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <div style={{ width: '550px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+}
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    children: 'Default Button'
+  }
+}
+Default.storyName = '默认按钮样式'
