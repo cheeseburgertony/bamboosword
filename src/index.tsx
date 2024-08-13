4491e3928d3804c18b79eb29b8dc4ebbd375fcaf
cheeseburgertony <1821955020@qq.com>
2024-08-14 01:15:54 +0800

    feat: Rollup打包UMD格式

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import './styles/index.scss'
+
 library.add(fas)
 
 export * from './components/Alert'
